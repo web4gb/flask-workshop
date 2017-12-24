734ccd1b648f1734b491c5ea79ac9e8f04b50c40
web4gb <34712534+web4gb@users.noreply.github.com>
2017-12-24 00:26:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+guesses = ['Python', 'Java', 'C++']
 
 @app.route('/')
 def index():
     return '<h1>Guess the Language!</h1>'
 
+@app.route('/guess/<int:id>')
+def guess(id):
+    return ('<h1>Guess the Language!</h1>'
+            '<p>My guess: {0}</p>').format(guesses[id])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
